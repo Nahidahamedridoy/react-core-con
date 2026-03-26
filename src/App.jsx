90cb9b3bae16094f23b8681a0de6755078ddd6d8
Nahidahamedridoy <nahidahamedridoy@gmail.com>
2026-03-26 15:27:33 +0600

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import Batsman from './batsman';
 import Baller from './boller';
 import Counter from './counter';
+import Users from './users';
+import Friends from './friends';
+import Posts from './posts';
+import Players from './players';
 import './App.css'
+import { Suspense } from 'react';
+
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(res => res.json())
+
+// const fetchFriends = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//   return res.json();
+// }
+
+// const fetchPosts = async() =>{
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   return res.json();
+// }
 
 function App() {
 
-  function handleClick(){
+  // const friendsPromise = fetchFriends();
+  // const postsPromise = fetchPosts();
+
+  function handleClick() {
     alert('I am Clicked.')
   }
 
@@ -13,7 +34,7 @@ function App() {
     alert('clicked 3')
   }
 
-  const handleAdd5 = (num)=>{
+  const handleAdd5 = (num) => {
     const newNum = num + 5;
     alert(newNum);
   }
@@ -21,20 +42,34 @@ function App() {
   return (
     <>
       <section id="center">
-        
+
         <div>
           <h1>Get started</h1>
+
+          <Players></Players>
+
+          {/* <Suspense fallback={<h4>Posts Are Coming....</h4>}>
+           <Posts postsPromise={postsPromise}></Posts>
+          </Suspense> */}
+
+          {/* <Suspense fallback={<h3>Loading...</h3>}>
+            <Users fetchUsers={fetchUsers}></Users>
+          </Suspense> */}
+
+          {/* <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+            <Friends friendsPromise={friendsPromise}></Friends>
+          </Suspense> */}
 
           <Batsman></Batsman>
           <Baller></Baller>
           <Counter></Counter>
-          
+
           {/* <button onclick="handleClick()">Click me</button> */}
           {/* btn system-1 */}
           <button onClick={handleClick}>Click me</button>
 
           {/* btn system-2 */}
-          <button onClick={function handleClick2(){alert('clicked 2')}}>Click me 2</button>
+          <button onClick={function handleClick2() { alert('clicked 2') }}>Click me 2</button>
 
           {/* btn system-3 */}
           <button onClick={handleClick3}>Click me 3</button>
@@ -43,9 +78,9 @@ function App() {
           <button onClick={() => alert('clicked-4')}>Click me 4</button>
 
           <button onClick={() => handleAdd5(10)}>Click Add 5</button>
-         
+
         </div>
-        
+
       </section>
 
 
---
+++ src/friend.jsx
@@ -0,0 +1,13 @@
+export default 
+function Friend ({friend}){
+    console.log(friend);
+
+    const {name , email , phone} = friend;
+    return (
+        <div className="card">
+            <h4>Name: {name} </h4>
+            <h4>E-mail: {email} </h4>
+            <p>Phone: {phone}</p>
+        </div>
+    )
+}
